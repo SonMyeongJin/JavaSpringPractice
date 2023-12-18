cc4e26580893d774852885a7ef19609807579e32
SonMyeongJin <smj2802@naver.com>
2023-12-19 00:38:25 +0900

    first commit

---
+++ src/main/java/myeongjinGroup/SpringPractice/MyeongjinApplication.java
@@ -0,0 +1,13 @@
+package myeongjinGroup.SpringPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyeongjinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyeongjinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myeongjinGroup/SpringPractice/MyeongjinApplicationTests.java
@@ -0,0 +1,13 @@
+package myeongjinGroup.SpringPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyeongjinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
