c963b06200387cc8aaf1babc6903012e218c68fa
SonMyeongJin <smj2802@naver.com>
2024-01-02 22:54:23 +0900

    MVC Basic Init

---
+++ src/main/java/myeongjinGroup/SpringPractice/controller/HelloController.java
@@ -0,0 +1,23 @@
+package myeongjinGroup.SpringPractice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model)
+    {
+        model.addAttribute("data","Hello!");
+        return "hello1";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name" , required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+}
