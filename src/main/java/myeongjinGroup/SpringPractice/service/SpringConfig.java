d4064b862c44eb11adcd193d17546488e143a174
SonMyeongJin <smj2802@naver.com>
2024-01-12 17:59:05 +0900

    jdbc connect

--- src/main/java/myeongjinGroup/SpringPractice/service/SpringConfig.java
+++ src/main/java/myeongjinGroup/SpringPractice/service/SpringConfig.java
@@ -1,13 +1,23 @@
 package myeongjinGroup.SpringPractice.service;
 
-import myeongjinGroup.SpringPractice.domain.Member;
+import myeongjinGroup.SpringPractice.repository.JdbcMemberRepository;
 import myeongjinGroup.SpringPractice.repository.MemberRepository;
 import myeongjinGroup.SpringPractice.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +26,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+
     }
 }
